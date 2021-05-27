07d24c77e964883ce9a03d356fa8eb71af8bd713
Atanu Majee <atanumajee147@gmail.com>
2021-05-27 10:20:18 +0530

    First Project

---
+++ C_Projectas/Calculator/calculator.c
@@ -0,0 +1,33 @@
+# include<stdio.h>
+
+int main(){
+    char o;
+    int a;
+    int b;
+printf("Enter Your operator :");
+scanf("%c",&o);
+printf("Enter The Fst Opodants : ");
+scanf("%d",&a);
+printf("Enter The Second Opodants : ");
+scanf("%d",&b);
+switch (o)
+{
+case '+':
+    printf("Your answer is %d + %d = %d \n",a,b,a+b);
+    break;
+case '-':
+    printf("Your answer is %d - %d = %d \n",a,b,a-b);
+    break;
+case '*':
+    printf("Your answer is %d * %d = %d \n",a,b,a*b);
+    break;
+case '/':
+    printf("Your answer is %d / %d = %d \n",a,b,a/b);
+    break;
+
+default:
+printf(" Your Entry is rong ! \n");
+    break;
+}
+return 0;
+}
